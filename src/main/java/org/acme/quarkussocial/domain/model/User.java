ce53af787a676c5bb8d3f98fe8517593546a561a
jihye <jihye83@hotmail.com>
2023-05-23 16:36:32 -0300

    using lombok

--- src/main/java/org/acme/quarkussocial/domain/model/User.java
+++ src/main/java/org/acme/quarkussocial/domain/model/User.java
@@ -1,12 +1,11 @@
 package org.acme.quarkussocial.domain.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
-
-import java.util.Objects;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,40 +15,4 @@ public class User {
     @Column(name = "age")
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 }
--- src/main/java/org/acme/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/org/acme/quarkussocial/rest/dto/ResponseError.java
@@ -3,12 +3,14 @@ package org.acme.quarkussocial.rest.dto;
 import com.mysql.cj.exceptions.StreamingNotifiable;
 import jakarta.validation.ConstraintViolation;
 import jakarta.ws.rs.core.Response;
+import lombok.Data;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
 public class ResponseError {
 
     //criando um erro e dando um nome.
@@ -30,22 +32,6 @@ public class ResponseError {
 
         var responseError = new ResponseError(message, errors);
         return responseError;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Collection<FieldError> getErrors() {
-        return errors;
-    }
-
-    public void setErrors(Collection<FieldError> errors) {
-        this.errors = errors;
     }
 
     public Response withStatusCode(int code){
